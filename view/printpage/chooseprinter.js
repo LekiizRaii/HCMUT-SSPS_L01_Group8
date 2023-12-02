fe340498dbc39e792e8e4cf3f1e3bbd61ad7c111
huyhoang379 <hohuyhoang01@gmail.com>
2023-12-03 02:45:09 +0700

    Update view\printpage

--- view/printpage/chooseprinter.js
+++ view/printpage/chooseprinter.js
@@ -11,9 +11,9 @@ function loadPrintModal() {
             // Đang in
             var str = `
                 <div class="flex flex-col m-auto max-w-screen p-4">
-                    <h1 class="text-3xl font-normal m-auto dark:text-white mb-4">File của bạn đang được in...</h1>
+                    <h1 class="text-3xl text-center font-normal m-auto dark:text-white mb-4">File của bạn đang được in...</h1>
                     <img src="../img/printing.png" alt="Hình ảnh" class="w-6/12 m-auto my-3">
-                    <h1 class="text-3xl font-normal m-auto dark:text-white mt-4">Vui lòng chờ...</h1>
+                    <h1 class="text-3xl text-center font-normal m-auto dark:text-white mt-4">Vui lòng chờ...</h1>
                 </div>`;
             $('#bodyModal').html(str);
             $('#return-home').hide();
@@ -23,7 +23,7 @@ function loadPrintModal() {
                 // After the delay, transition to the "In thành công" state
                 var successStr = `
                     <div class="flex flex-col m-auto max-w-screen p-4">
-                        <h1 class="text-3xl font-normal m-auto dark:text-white mb-4">File của bạn đã được in thành công.</h1>
+                        <h1 class="text-3xl text-center font-normal m-auto dark:text-white mb-4">File của bạn đã được in thành công.</h1>
                         <img src="../img/happy-face.png" alt="Hình ảnh" class="w-6/12 m-auto my-3">
                     </div>`;
                 $('#bodyModal').html(successStr);
--- view/printpage/printpage.js
+++ view/printpage/printpage.js
@@ -33,14 +33,14 @@ function loadBugModal() {
             var str = '';
             str += `
                     <div class="flex flex-col m-auto max-w-screen p-4">
-                        <h1 class="text-2xl font-bold m-auto dark:text-white mb-3">LỖI CÀI ĐẶT IN</h1>
+                        <h1 class="text-2xl text-center font-bold m-auto dark:text-white mb-3">LỖI CÀI ĐẶT IN</h1>
                 
                         <img src="../img/error.png"
                         alt="Hình ảnh" class="w-7/12 m-auto my-3">
                 
-                        <p class="text-xl text-black m-auto dark:text-white mt-3">Các thông số cài đặt in không hợp lệ.</p>
+                        <p class="text-xl text-center text-black m-auto dark:text-white mt-3">Các thông số cài đặt in không hợp lệ.</p>
 
-                        <p class="text-xl text-black m-auto dark:text-white">Vui lòng kiểm tra lại.</p>
+                        <p class="text-xl text-center text-black m-auto dark:text-white">Vui lòng kiểm tra lại.</p>
                     </div>   
                     `
             $('#bodyModal').html(str);
@@ -49,14 +49,14 @@ function loadBugModal() {
             var str = '';
             str += `
                     <div class="flex flex-col m-auto max-w-screen p-4">
-                        <h1 class="text-2xl font-bold m-auto dark:text-white mb-3">KHÔNG ĐỦ GIẤY IN</h1>
+                        <h1 class="text-2xl text-center font-bold m-auto dark:text-white mb-3">KHÔNG ĐỦ GIẤY IN</h1>
                 
                         <img src="../img/notenoughpage.png"
                         alt="Hình ảnh" class="w-4/12 m-auto my-3">
                 
-                        <p class="text-xl text-black m-auto dark:text-white mt-3">Bạn không có đủ giấy để in file này.</p>
+                        <p class="text-xl text-center text-black m-auto dark:text-white mt-3">Bạn không có đủ giấy để in file này.</p>
 
-                        <p class="text-xl text-black m-auto dark:text-white">Vui lòng mua thêm giấy in hoặc điều chỉnh lại cài đặt.</p>
+                        <p class="text-xl text-center text-black m-auto dark:text-white">Vui lòng mua thêm giấy in hoặc điều chỉnh lại cài đặt.</p>
                     </div>   
                     `
             $('#bodyModal').html(str);
