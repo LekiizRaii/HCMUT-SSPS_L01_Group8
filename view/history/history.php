21abeabbd4112c1d328423a00844a84c2b5c3982
huyhoang379 <hohuyhoang01@gmail.com>
2023-12-07 12:22:57 +0700

    Update Change link .html to .php

--- view/history/history.php
+++ view/history/history.php
@@ -36,7 +36,7 @@ require_once '../../db/db_connection.php';
 <body class="bg-gray-100 :dark:bg-gray-900">
     <nav class="bg-white border-gray-200 :dark:bg-gray-900">
         <div class="max-w-screen-full flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="../homepage/homepage.html" class="ml-5 flex items-center space-x-3 rtl:space-x-reverse">
+            <a href="../homepage/homepage.php" class="ml-5 flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="../img/hcmut.png" class="h-8" alt="HCMUT Logo" />
                 <div class="ml-4">
                     <div class="self-center text-base font-semibold whitespace-nowrap :dark:text-white">Trường Đại học 
